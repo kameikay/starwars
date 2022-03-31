329dc27db5ceefc45b76e500d1fd4f9dfc738892
Victor Kamei Kay <victorkameikay@Victors-MacBook-Pro.local>
2022-03-30 22:23:54 -0300

    add input search at home page

---
+++ src/components/InputSearch.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const InputSearch = styled.input`
+  width: 32rem;
+  height: 3rem;
+  border-radius: 0.5rem;
+  border: 2px solid ${({ theme }) => theme.colors.light[900]};
+  padding: 0 1rem;
+  outline: none;
+  transition: all 0.3s ease-in;
+  background-color: ${({ theme }) => theme.colors.light[900]};
+
+  &:focus {
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+  }
+`;
